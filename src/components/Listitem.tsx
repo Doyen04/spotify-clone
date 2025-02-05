989c5879bec8b96f154660802c6b0381f29dbca4
Doyen <101886678+Doyen04@users.noreply.github.com>
2025-02-05 23:45:23 +0100

    Update ListItem component to include image and title; modify Home page to pass props to ListItem;

--- src/components/Listitem.tsx
+++ src/components/Listitem.tsx
@@ -2,27 +2,35 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
 import { twMerge } from "tailwind-merge"
-
 
 interface ListItemProps {
     image: string;
     name: string;
-    href:string;
-    className: string
+    href: string;
+    className?: string
 }
 
-const ListItem: React.FC<ListItemProps> = ({image, name, href, className}) => {
+const ListItem: React.FC<ListItemProps> = ({ image, name, href, className }) => {
     const router = useRouter()
 
-    const onClick = ()=>{
+    const onClick = () => {
         router.push(href)
     }
     // TODO MEANING GROUP
-    return  (
-        <button className={twMerge(`relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4`, className)}>
+    return (
+        <button onClick={onClick} title={name} className={twMerge(`relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4`, className)}>
             <div className="relative min-h-[64px] min-w-[64px]">
-                {/* <Image /> */}
+                <Image src={image} alt="image" className="object-cover" fill />
+            </div>
+            <p className="font-medium truncate py-5">
+                {name}
+            </p>
+            <div className="absolute transition opacity-0 rounded-full flex items-center 
+            justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 
+            hover:scale-110">
+                <FaPlay className="text-black" />
             </div>
         </button>
     )
