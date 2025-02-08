94f7b2baccd038d45322c58e7f26d298839b0a96
Doyen <101886678+Doyen04@users.noreply.github.com>
2025-02-08 23:29:52 +0100

    Remove obsolete VSCode configuration files and update package dependencies; integrate Supabase Auth UI in AuthModal component

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -1,13 +1,33 @@
 "use client"
 
+import { useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { Auth } from "@supabase/auth-ui-react"
+
 import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 const AuthModal = () => {
-    return(
-        <Modal title={"Welcome back"} description={"Login to your account"} isOpen onChange={()=>{}}>
-            auth
+    const supabaseClient = useSupabaseClient()
+    const router = useRouter()
+    const { session } = useSessionContext() 
+
+    return (
+        <Modal title={"Welcome back"} description={"Login to your account"} isOpen onChange={() => { }}>
+            <Auth theme={'dark'} providers={['github']} supabaseClient={supabaseClient}
+                appearance={{
+                    theme: ThemeSupa,
+                    variables: {
+                        default: {
+                            colors: {
+                                brand: '#404040',
+                                brandAccent: '#22c55e'
+                            }
+                        }
+                    }
+                }} />
         </Modal>
-  )
+    )
 }
 
 export default AuthModal
